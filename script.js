123523735688d833b430f3cb9801d329491c2ebd
Pushpendra Chaudhary <120740536+Pushpendrachaudhary88@users.noreply.github.com>
2023-06-22 13:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function threeSum(arr, target) {
+// function threeSum(arr, target) {
 // write your code here
-  
+	function decimalToBinary(decimal) {
+  // Check if the input is 0
+  if (decimal === 0) {
+    return '0';
+  }
+
+  let binary = '';
+
+  while (decimal > 0) {
+    // Append the least significant bit (remainder of division by 2)
+    binary = (decimal % 2) + binary;
+
+    // Update the decimal by dividing it by 2 (integer division)
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
 }
+  
+
 
 module.exports = threeSum;
