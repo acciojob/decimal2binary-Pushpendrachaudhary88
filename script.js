b67cd2d0af2b39f5196a7097b2a24abf224c928a
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:09:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-// function threeSum(arr, target) {
-// write your code here
-	function decimalToBinary(decimal) {
-  // Check if the input is 0
-  if (decimal === 0) {
-    return '0';
-  }
-
-  let binary = '';
-
-  while (decimal > 0) {
-    // Append the least significant bit (remainder of division by 2)
-    binary = (decimal % 2) + binary;
-
-    // Update the decimal by dividing it by 2 (integer division)
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
+function decimalToBinary(num) {
+  // Write your code here
 }
-  
 
-
-module.exports = threeSum;
+window.decimalToBinary = decimalToBinary;
